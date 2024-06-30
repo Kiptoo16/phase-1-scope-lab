dca190ead97f760886e31f0250f60342c2806b32
Kevin Langat <kevin.langat@student.moringaschool.com>
2024-06-30 15:10:58 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'; 
+}
+
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'; 
+}
+
+const leastFavoriteCustomer = 'someone'; 
+
+function changeLeastFavoriteCustomer() {
+  
+  leastFavoriteCustomer = 'new person'; 
+}
+
+console.log(customerName); bob
+upperCaseCustomerName();
+console.log(customerName); BOB
+
+setBestCustomer();
+console.log(bestCustomer); bob
+
+overwriteBestCustomer();
+console.log(bestCustomer); bob
+
+console.log(leastFavoriteCustomer); 
